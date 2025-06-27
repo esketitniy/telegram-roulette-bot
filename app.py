e59a84965b1add86b1cc719c894a5f198bb75dd3
esketitniy <esketitniy7@mail.ru>
2025-06-27 20:17:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -413,4 +413,4 @@ if __name__ == '__main__':
     # Запускаем игровой цикл
     start_game_loop()
     
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
