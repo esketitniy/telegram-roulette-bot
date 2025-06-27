c79fd50ae52687931f0ea434bf7c76dafa46d83a
esketitniy <esketitniy7@mail.ru>
2025-06-27 16:02:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Модели базы данных
 class User(db.Model):
